5e99658acff293d873c529a060ac890fddadbdc8
deethesaint <vtduong0912@gmail.com>
2024-08-17 10:06:57 +0700

    connection string modified

--- XuongMay_BE/Program.cs
+++ XuongMay_BE/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

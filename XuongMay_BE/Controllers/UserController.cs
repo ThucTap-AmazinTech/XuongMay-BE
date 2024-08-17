eeeb787344a065924fcbe229464335deef680a80
deethesaint <vtduong0912@gmail.com>
2024-08-17 21:50:03 +0700

    mosture

--- XuongMay_BE/Controllers/UserController.cs
+++ XuongMay_BE/Controllers/UserController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using XuongMay_BE.Data;
+using XuongMay_BE.Models;
 
 namespace XuongMay_BE.Controllers
 {
@@ -7,5 +10,59 @@ namespace XuongMay_BE.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly DataContext _context;
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetAllUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user is null)
+                return NotFound("User not found!");
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> AddUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<User>> UpdateUser(User user)
+        {
+            var dbUser = await _context.Users.FindAsync(user.Id);
+            if (dbUser is null)
+                return NotFound("User not found!");
+            dbUser.Name = user.Name;
+            dbUser.Email = user.Email;
+            dbUser.Password = user.Password;
+            dbUser.Image = user.Image;
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Users.FindAsync(user.Id));
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<User>>> DeleteUser(int id)
+        {
+            var dbUser = await _context.Users.FindAsync(id);
+            if (dbUser is null)
+                return NotFound("User not found!");
+            _context.Users.Remove(dbUser);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Users.ToListAsync());
+        }
     }
 }

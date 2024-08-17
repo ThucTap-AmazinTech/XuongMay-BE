dcbbeca9c307f32dfe269d99a6a53176ae928135
NhatVy2812 <166157249+NhatVy2812@users.noreply.github.com>
2024-08-17 13:45:11 +0700

    add product

---
+++ XuongMay_BE/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XuongMay_BE.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

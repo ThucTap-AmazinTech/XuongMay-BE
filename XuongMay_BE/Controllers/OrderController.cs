7ae8e3e958c5058a6a359a4f42876f1b02496fbe
Nguyễn Phương Điền <diennguyenphuong2003@gmail.com>
2024-08-17 12:34:26 +0700

    CRUD Customer

---
+++ XuongMay_BE/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XuongMay_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+    }
+}

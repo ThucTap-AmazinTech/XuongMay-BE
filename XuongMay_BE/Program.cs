0f276a175cb20a1646ca43ff99eb1e7cefa6c9f5
deethesaint <vtduong0912@gmail.com>
2024-08-17 10:09:00 +0700

    updated

--- XuongMay_BE/Program.cs
+++ XuongMay_BE/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using XuongMay_BE.Data;
 using XuongMay_BE.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
